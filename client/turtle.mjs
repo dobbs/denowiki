1ce9598d5774dae936c5138464286c03287c6577
Eric Dobbs <edobbs@newrelic.com>
2020-03-30 09:24:46 -0600

    Implement /turtle/save endpoint in localhost.ts

--- client/turtle.mjs
+++ client/turtle.mjs
@@ -1,7 +1,40 @@
 import {TurtleCanvas} from 'http://turtle.simple.localtest.me/assets/turtle/turtle-canvas.js';
 
 export class Turtle extends TurtleCanvas {
+  connectedCallback() {
+    this.draw();
+  }
   render(json) {
+    this.id = json.id;
+    this.text = "Turtle wants to be here";
+    this.turn().move().turn().nextMovesize().move();
+    this.history = this.turtle.history;
+    this.save();
+  }
+
+  get json() {
+    return {
+      id: this.id,
+      type: "turtle",
+      text: this.text,
+      history: this.history
+    }
+  }
+
+  async save() {
+    try {
+      this.style.backgroundColor = "rgba(255, 0, 0, 0.25)"
+      let resp = await fetch(`/turtle/save`, {method: "POST", body: JSON.stringify(this.json)})
+      let json = await resp.json()
+      if (json.success) {
+        this.style.backgroundColor = ""
+        return
+      }
+      console.log("Unable to save", json)
+    } catch(e) {
+      console.log("Unable to save", e)
+    }
+
   }
 }
 registerPlugin("turtle", Turtle);
--- meta-sites/localhost.ts
+++ meta-sites/localhost.ts
@@ -38,6 +38,42 @@ export async function init({site, system}: {site: MetaSite, system: System}) {
   }
 }
 
+function wantsTurtleSave(req: Request) {
+  return req.url.indexOf("/turtle/save") == 0;
+}
+let turtle = {
+  saved: {
+    history: [{"fn":"clear","beforestate":{},"state":{"name":"origin","x":0,"y":0,"direction":-1.5707963267948966,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6}},{"fn":"turn","beforestate":{"name":"origin","x":0,"y":0,"direction":-1.5707963267948966,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6},"state":{"name":"origin","x":0,"y":0,"direction":5.759586531581287,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6}},{"fn":"move","beforestate":{"name":"origin","x":0,"y":0,"direction":5.759586531581287,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6},"state":{"name":"origin","x":25.980762113533153,"y":-15.000000000000014,"direction":5.759586531581287,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6}},{"fn":"turn","beforestate":{"name":"origin","x":25.980762113533153,"y":-15.000000000000014,"direction":5.759586531581287,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6},"state":{"name":"origin","x":25.980762113533153,"y":-15.000000000000014,"direction":0.5235987755982983,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6}},{"fn":"nextMovesize","beforestate":{"name":"origin","x":25.980762113533153,"y":-15.000000000000014,"direction":0.5235987755982983,"pensize":1,"pencolor":"black","movesize":30,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6},"state":{"name":"origin","x":25.980762113533153,"y":-15.000000000000014,"direction":0.5235987755982983,"pensize":1,"pencolor":"black","movesize":35,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6}},{"fn":"move","beforestate":{"name":"origin","x":25.980762113533153,"y":-15.000000000000014,"direction":0.5235987755982983,"pensize":1,"pencolor":"black","movesize":35,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6},"state":{"name":"origin","x":56.29165124598852,"y":2.499999999999968,"direction":0.5235987755982983,"pensize":1,"pencolor":"black","movesize":35,"turnsize":1.0471975511965976,"turnsizeNumerator":1,"turnsizeDenominator":6}}]
+  }
+};
+async function turtleSave(req: Request) {
+  if (req.method != "POST") {
+    // Different status code?
+    wiki.serve404(req);
+    return
+  }
+  // building a JSON blob from req.body.read 'cos I can't find a
+  // higher-level API. but seems hard to believe I haven't missed
+  // something obvious.
+  // copied a bunch of this from deno's http server source:
+  // https://github.com/denoland/deno/blob/bced52505f32d6cca4f944bb610a8a26767908a8/std/http/server.ts#L57-L66
+  const buf = new Uint8Array(req.contentLength);
+  let bufSlice = buf;
+  let totRead = 0;
+  while (true) {
+    const nread = await req.body.read(bufSlice);
+    if (nread === Deno.EOF) break;
+    totRead += nread;
+    if (totRead >= req.contentLength) break;
+    bufSlice = bufSlice.subarray(nread);
+  }
+  let body = JSON.parse(String.fromCharCode(...buf))
+  console.log({where: 'POST /turtle/save', id:body.id, historyLength:body.history.length});
+  turtle[body.id] = body;
+  wiki.serveJson(req, {success: true, ...body});
+  return;
+}
+
 export async function serve(req: Request, system: System) {
   if (req.url == "/welcome-visitors.json") {
     wiki.serveJson(
@@ -71,7 +107,7 @@ export async function serve(req: Request, system: System) {
       req,
       wiki.page("Wander", [
         wiki.item("turtle-wander", {}),
-        wiki.item("turtle", {})
+        wiki.item("turtle", turtle.saved)
       ])
     );
   } else if (metaPages[req.url]) {
@@ -79,6 +115,8 @@ export async function serve(req: Request, system: System) {
     console.log("calling:", metaPages[req.url]);
     let data = await metaPages[req.url](req, system);
     wiki.serveJson(req, data);
+  } else if (wantsTurtleSave(req)) {
+    return turtleSave(req)
   } // This will serve system urls
   else {
     wiki.serve(req, system);
